8115e5d558b5c1820b2006ab81487fc6f430e66f
omgurusai kanithi <oomgurusai@gmail.com>
2025-07-04 10:54:39 +0530

    Add files via upload

---
+++ pgrm3.py
@@ -0,0 +1,11 @@
+def reverseArray(arr):
+    i = 0
+    j = len(arr) - 1
+    while i < j:
+        arr[i], arr[j] = arr[j], arr[i]
+        i += 1
+        j -= 1
+
+arr = list(map(int, input("Enter elements: ").split()))
+reverseArray(arr)
+print("Reversed Array:", arr)
